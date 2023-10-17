7f9185bb85977940474e9d4a2906f1d84e749be1
Imtiaz Ahammed <142397687+Imtiaz575@users.noreply.github.com>
2023-10-17 09:05:18 +0530

    Add files via upload

---
+++ SearchDom.js
@@ -0,0 +1,29 @@
+//let ctittle = document.getElementsByClassName("card-title")[0]
+//ctittle.style.color="red"
+//let ctit = document.getElementsByClassName("card-text")[0]
+//ctit.style.color="red"
+
+let ctittles=document.querySelectorAll(".card-title")
+ctittles[0].style.color="green"
+ctittles[1].style.color="blue"
+ctittles[2].style.color="red"
+console.log(ctittles)
+let ctext=document.querySelectorAll(".card-text")
+ctext[0].style.color="black"
+ctext[1].style.color="black"
+ctext[2].style.color="black"
+
+/*let ctexts=document.querySelectorAll(".card-text")
+ctexts[0].style.background="blue"
+ctexts[1].style.background="red"
+ctexts[2].style.background="violet"*/
+
+
+let contexs=document.querySelectorAll(".this")
+contexs[0].style.background="yellow"
+contexs[1].style.background="red"
+contexs[2].style.background="violet"
+contexs[0].style.color="black"
+
+
+
